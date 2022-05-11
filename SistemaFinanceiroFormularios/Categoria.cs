131a8fa12c60b3099eb76ee75f3411ad38bc5cf0
AmeMeida <ame.pistache@gmail.com>
2022-05-11 16:52:28 -0300

    voce commitaria voce commitaria essa categoria xoxa capenga raquitica abstrata insignificante minúscula que nao serve pra nada voce comitaria

--- SistemaFinanceiroFormularios/Categoria.cs
+++ SistemaFinanceiroFormularios/Categoria.cs
@@ -1,131 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace SistemaFinanceiroFormularios
 {
-    public partial class Categoria : Form
+    public class Categoria
     {
-        private bool Insercao = false;
-        private bool Edicao = false;
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+        public int Tipo { get; set; }
+        public int Status { get; set; }
+
+        public List<Categoria> _lstCategoria = new List<Categoria>();
 
         public Categoria()
         {
-            InitializeComponent();
-            
+
         }
 
-        private void Categoria_FormClosing(object sender, FormClosingEventArgs e)
+        public Categoria( int id, string nome, string descricao, int tipo, int status)
         {
-            if (Edicao || Insercao)
-            {
-                e.Cancel = true;
-                MessageBox.Show("Rimani qui!", "Aviso do sistema!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            Tipo = tipo;
+            Status = status;
         }
 
-        private void CamposLimpar()
+        public void AddToList(int id, string nome, string descricao, int tipo, int status)
         {
-            categoriaTextNome.Clear();
-            categoriaTextDescricao.Clear();
-            categoriaRadioTipoDespesa.Checked = false;
-            categoriaRadioTipoReceita.Checked = false;
-            categoriaCheckStatus.Checked = false;
+            _lstCategoria.Add(new Categoria(id, nome, descricao, tipo, status));
         }
 
-        private void CamposReset()
+        public List<Categoria> ToList()
         {
-            categoriaGroupInfo.Enabled = false;
-            categoriaBotaoAlterar.Enabled = true;
-            categoriaBotaoNovo.Enabled = true;
-            categoriaBotaoCancelar.Visible = false;
-            categoriaBotaoSalvar.Visible = false;
-            categoriaBotaoExcluir.Visible = true;
-            Insercao = false;
-            Edicao = false;
-
-            categoriaBotaoNovo.Focus();
+            return _lstCategoria;
         }
 
-        private void categoriaBotaoNovo_Click(object sender, EventArgs e)
+        public List<Categoria> GeraCategorias()
         {
-            categoriaGroupInfo.Enabled = true;        // Ativar a caixa de grupo
-            CamposLimpar();                           // Limpar campos do formulário
-            categoriaTextNome.Focus();                // Foco no campo Nome
-            categoriaBotaoNovo.Enabled = false;       // Desabilitar botão novo
-            categoriaBotaoAlterar.Enabled = false;    // Desabiitar botão alterar
-            categoriaBotaoCancelar.Visible = true;    // Tornar visível o botão cancelar
-            categoriaBotaoSalvar.Visible = true;      // Tornar visível o botão salvar
-            categoriaBotaoExcluir.Visible = false;    // Ocultar o botão excluir
+            AddToList(1, "Salário", "Salário da UNICAMP", 1, 1);
 
-            categoriaCheckStatus.Checked = true;      // Automaticamente define o status como ativo
-            
-            // Definição das variáveis booleanas da classe
-            Insercao = true;
-            Edicao = false;
-        }
-        private void categoriaBotaoAlterar_Click(object sender, EventArgs e)
-        {
-            categoriaGroupInfo.Enabled = true;        // Ativa a caixa de grupo
-            categoriaTextNome.Focus();                // Foco no campo Nome
-            categoriaBotaoAlterar.Enabled = false;    // Desabilitar o botão alterar
-            categoriaBotaoNovo.Enabled = false;       // Ocultar o botão novo
-            categoriaBotaoCancelar.Visible = true;    // Tornar visível o botão cancelar
-            categoriaBotaoSalvar.Visible = true;      // Tornar visível o botão salvar
-            categoriaBotaoExcluir.Visible = false;    // Ocultar o botão excluir
-
-            // Definição das variáveis booleanas da classe
-            Insercao = true;
-            Edicao = false;
-        }
-
-
-        private void categoriaBotaoExcluir_Click(object sender, EventArgs e)
-        {
-            if (MessageBox.Show("Deseja mesmo excluir essa entrada?", "Mensagem do sistema ", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
-            {
-                CamposLimpar();
-                MessageBox.Show("Registro excluído com sucesso.", "Aviso do sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                categoriaBotaoNovo.Focus();
-            }
-        }
-
-
-        private void categoriaBotaoCancelar_Click(object sender, EventArgs e)
-        {
-            if (MessageBox.Show("Deseja mesmo cancelar?", "Mensagem do sistema ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                CamposLimpar();
-                CamposReset();
-
-                Insercao = true;
-                Edicao = false;
-            }
-        }
-        
-        private void Categoria_Load(object sender, EventArgs e)
-        {
-            categoriaTextNome.Text = "Combustível";
-            categoriaTextDescricao.Text = "Consumo de combustivéis em um ano pelo consumidor";
-            categoriaRadioTipoDespesa.Checked = true;
-            categoriaCheckStatus.Checked = true;
-        }
-
-        private void categoriaBotaoSalvar_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Registro salvo com sucesso!", "Aviso do sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            CamposReset();
-        }
-
-        private void categoriaCheckStatus_CheckedChanged(object sender, EventArgs e)
-        {
-
+            AddToList(1, "Combustível", "Despesas com combustível", 2, 1);
+            return _lstCategoria;
         }
     }
 }
